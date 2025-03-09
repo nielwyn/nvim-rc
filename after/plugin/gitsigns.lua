4129c1c5cf66b4ab8ef2bcfb92eb5733ea4385d6
nielwyn <nghoangnghiabt@gmail.com>
2025-03-09 22:48:25 +0700

    add gitsigns keymap

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -23,6 +23,29 @@ gitsigns.setup {
 		delay = 0,
 	},
 	on_attach = function()
-		vim.keymap.set('n', '<leader>ph', gitsigns.preview_hunk)
-	end
+		vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk)
+	end,
+
+	-- Navigation
+	vim.keymap.set('n', ']c', function()
+		if vim.wo.diff then
+			vim.cmd.normal({ ']c', bang = true })
+		else
+			gitsigns.nav_hunk('next')
+		end
+	end),
+
+	vim.keymap.set('n', '[c', function()
+		if vim.wo.diff then
+			vim.cmd.normal({ '[c', bang = true })
+		else
+			gitsigns.nav_hunk('prev')
+		end
+	end),
+
+	-- Actions
+	vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk),
+	vim.keymap.set('v', '<leader>hr', function()
+		gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+	end)
 }
