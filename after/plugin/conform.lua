bceb3a2030d1165f35cdb53998967bd299885e26
nielwyn <nghoangnghiabt@gmail.com>
2025-05-15 13:42:58 +0700

    update conform config

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,40 +1,34 @@
 local conform = require("conform")
 
 conform.setup({
-	formatters_by_ft = {
-		javascript = { "prettier" },
-		typescript = { "prettier" },
-		javascriptreact = { "prettier" },
-		typescriptreact = { "prettier" },
-		css = { "prettier" },
-		html = { "prettier" },
-		json = { "prettier" },
-		yaml = { "prettier" },
-		lua = { "stylua" },
-		python = { "isort", "black" },
-	},
-	opts = {
-		-- Define formatters
-		formatters_by_ft = {
-			lua = { "stylua" },
-			python = { "isort", "black" },
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-		},
-		-- Set default options
-		default_format_opts = {
-			lsp_format = "fallback",
-		},
-		-- Customize formatters
-		formatters = {
-			shfmt = {
-				prepend_args = { "-i", "2" },
-			},
-		},
-	},
+  formatters_by_ft = {
+    javascript = { "prettierd", "prettier", stop_after_first = true },
+    typescript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescriptreact = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    json = { "prettier" },
+    yaml = { "prettier" },
+    lua = { "stylua" },
+    python = { "isort", "black", stop_after_first = true },
+  },
+  -- Set default options
+  default_format_opts = {
+    lsp_format = "fallback",
+  },
+  -- Customize formatters
+  notify_on_error = true,
+  notify_no_formatters = true,
+  formatters = {
+    shfmt = {
+      prepend_args = { "-i", "2" },
+    },
+  },
 })
 
-vim.keymap.set({ "n", "v" }, "<leader>bf", function()
-	require("conform").format({ async = true }, function(err)
+vim.keymap.set("", "<leader>bf", function()
+  require("conform").format({ async = true }, function(err)
     if not err then
       local mode = vim.api.nvim_get_mode().mode
       if vim.startswith(string.lower(mode), "v") then
--- lua/nielwyn/options.lua
+++ lua/nielwyn/options.lua
@@ -9,6 +9,7 @@ vim.opt.signcolumn = 'yes'
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
 
 vim.wo.foldmethod = 'expr'
 vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
